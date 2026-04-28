82c6f0d452d6c04c3e1c936a32277f794597ee1d
Robert Vitonsky <robert@vitonsky.net>
2026-04-28 13:35:10 +0200

    docs: reuse docs
    Now README.md will be hosted in repo root.
    The tsdown while build will copy content in intermediate dir for publish

--- packages/transly/tsdown.config.ts
+++ packages/transly/tsdown.config.ts
@@ -1,4 +1,15 @@
+import fs from 'fs';
+import path from 'path';
 import { defineConfig } from 'tsdown';
+
+const root = process.cwd();
+
+const publishDir = path.join(root, '.publish');
+const distDir = path.join(root, 'dist');
+
+function copyRecursive(src: string, dest: string) {
+	fs.cpSync(src, dest, { recursive: true });
+}
 
 export default defineConfig({
 	entry: ['src/cli.ts', 'src/index.ts'],
@@ -9,4 +20,31 @@ export default defineConfig({
 		chunkFileNames: '[name].js',
 		assetFileNames: '[name][extname]', // ← key line
 	},
+	plugins: [
+		{
+			name: 'prepare-publish',
+			closeBundle() {
+				fs.rmSync(publishDir, { recursive: true, force: true });
+				fs.mkdirSync(publishDir, { recursive: true });
+
+				// move dist → publish
+				copyRecursive(distDir, path.join(publishDir, 'dist'));
+
+				// copy metadata
+				for (const file of [
+					'package.json',
+					{ from: '../../README.md', to: 'README.md' },
+				]) {
+					const { from, to } =
+						typeof file === 'string' ? { from: file, to: file } : file;
+
+					const src = path.resolve(path.join(root, from));
+					if (!fs.existsSync(src))
+						throw new Error(`File "${src}" is not found`);
+
+					fs.copyFileSync(src, path.join(publishDir, to));
+				}
+			},
+		},
+	],
 });
