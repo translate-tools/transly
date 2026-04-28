e0db791edeb695dbd2ebd1d0e2e5afede70a6db7
Robert Vitonsky <robert@vitonsky.net>
2026-04-28 12:29:12 +0200

    chore: delete stylelint

--- stylelint.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('stylelint').Config} */
-export default {
-	extends: ['stylelint-config-standard'],
-	rules: {
-		'selector-class-pattern': '.+',
-	},
-	ignoreFiles: ['**/*.ts', '**/*.tsx'],
-};
