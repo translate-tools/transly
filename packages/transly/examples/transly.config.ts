0760c9d26809fd216b366ca39a63959d7727fda3
Robert Vitonsky <robert@vitonsky.net>
2026-04-28 16:21:22 +0200

    chore: introduce CLI examples

---
+++ packages/transly/examples/transly.config.ts
@@ -0,0 +1,24 @@
+import 'dotenv/config';
+
+import { defineConfig } from '../src';
+
+export const waitRandom = async (minMs = 50, maxMs = 3000) => {
+	const delay = Math.floor(Math.random() * (maxMs - minMs + 1)) + minMs;
+	return new Promise((resolve) => setTimeout(resolve, delay));
+};
+
+export default defineConfig({
+	debug: true,
+
+	sourceLang: 'en',
+	targetLangs: ['cs', 'da', 'de', 'es', 'fr', 'hu', 'it', 'ja'],
+	localesDir: './locales',
+
+	cacheDir: './locales/.transly',
+	async translateChunk(items, language) {
+		await waitRandom();
+		return Object.fromEntries(
+			items.map((item) => [item.key, `[${language}] ${item.value}`]),
+		);
+	},
+});
